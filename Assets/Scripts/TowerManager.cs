6e52af92ecab2df5afc4208b415760f39a4803b5
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2021-10-23 19:17:08 +0600

    Tower building
    Now you can place tower on tower spots

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class TowerManager : MonoBehaviour
 {
-    TowerButton towerButtonPressed;
+    public TowerButton towerButtonPressed = null;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
@@ -16,12 +16,36 @@ public class TowerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
+
+
+            if(hit.collider.tag == "TowerSpot")
+            {
+                PlaceTower(hit);
+            }
+        }
+    }
+
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if(towerButtonPressed != null)
+        {
+            GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+            towerButtonPressed = null;
+        } else
+        {
+            Debug.Log("Please choose tower");
+        }
+
     }
 
     public void SelectedTower(TowerButton towerSeleted)
     {
         towerButtonPressed = towerSeleted;
-        Debug.Log("Pressed" + towerButtonPressed.gameObject);
+        Debug.Log("Pressed " + towerButtonPressed);
     }
 }
